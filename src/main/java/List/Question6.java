7010916a04bc8d310be17f2cce47e1c787719008
Hamza Demir <hamzademir4772@hotmail.com>
2018-12-27 17:26:25 +0000

    Completed Question 6

---
+++ src/main/java/List/Question6.java
@@ -0,0 +1,26 @@
+package List;
+
+//6. Write a Java program to remove the third element from a array list.
+
+import java.util.ArrayList;
+
+public class Question6 {
+
+    public static void main(String[] args){
+
+        ArrayList<String> colors = new ArrayList<String>();
+
+        colors .add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors .add("Blue");//
+        colors.add("Purple");
+        colors.add("White");
+
+        colors.remove(3);
+
+        System.out.println(colors);
+
+
+    }
+}
