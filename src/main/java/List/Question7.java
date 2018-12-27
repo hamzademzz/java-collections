f64e1bd2cb37ed5570cd9fe7b1deb80dd3516c85
Hamza Demir <hamzademir4772@hotmail.com>
2018-12-27 17:28:45 +0000

    Completed Question 7

---
+++ src/main/java/List/Question7.java
@@ -0,0 +1,20 @@
+package List;
+
+//7. Write a Java program to search an element in a array list
+
+import java.util.ArrayList;
+
+public class Question7 {
+
+    public static void main(String[] args){
+
+
+        ArrayList<String> colors = new ArrayList<String>();
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+
+        System.out.println(colors.contains("Red"));
+    }
+}
