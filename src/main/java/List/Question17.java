fcfd9bc2dc21a9fc805200105f6cf6059557e1a5
Hamza Demir <hamzademir4772@hotmail.com>
2018-12-27 23:56:00 +0000

    Completed Question 17

---
+++ src/main/java/List/Question17.java
@@ -0,0 +1,30 @@
+package List;
+
+//17. Write a Java program to empty an array list.
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Question17 {
+
+    public static void main(String[] args){
+
+        ArrayList<String> colors = new ArrayList<String>();
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Blue");
+        colors.add("White");
+        colors.add("Black");
+
+        System.out.println("Before removal" + colors);
+
+        colors.removeAll(colors);
+
+        System.out.println("After removal " + colors);
+
+
+
+    }
+}
