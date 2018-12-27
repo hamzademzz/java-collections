fa8a57310a25b71cc6ef1d437c4e16641424fa74
Hamza Demir <hamzademir4772@hotmail.com>
2018-12-27 17:37:24 +0000

    Completed Question 8

---
+++ src/main/java/List/Question8.java
@@ -0,0 +1,24 @@
+package List;
+
+//8. Write a Java program to sort a given array list.
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Question8 {
+
+    public static void main(String[] args){
+
+        ArrayList<String> colors = new ArrayList<String>();
+
+        colors .add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+
+        System.out.println(colors);
+        Collections.sort(colors);
+        System.out.println(colors);
+
+    }
+}
