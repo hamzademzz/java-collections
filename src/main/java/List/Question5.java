90b09a33282a63543aa76ba2c9d80aadce68a81f
Hamza Demir <hamzademir4772@hotmail.com>
2018-12-27 17:15:29 +0000

    Completed Question 5

---
+++ src/main/java/List/Question5.java
@@ -0,0 +1,25 @@
+package List;
+
+//5. Write a Java program to update specific array element by given element.
+
+import java.util.ArrayList;
+
+public class Question5 {
+
+    public static void main(String[] args){
+
+        ArrayList<String> colors = new ArrayList<String>();
+
+
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+
+        System.out.println(colors);
+        colors.set(1, "Updated Color");
+        System.out.println(colors);
+
+
+    }
+}
