ea3f5ad0bd2bb58f8530256f09892db00c7d3918
Hamza Demir <hamzademir4772@hotmail.com>
2018-12-27 14:17:13 +0000

    Created a new ArrayList

---
+++ src/main/java/List/Question1.java
@@ -0,0 +1,16 @@
+package List;
+
+//1. Write a Java program to create a new array list, add some colors (string) and print out the collection.
+
+import java.util.ArrayList;
+
+public class Question1 {
+
+    public static void main(String[] args){
+
+        ArrayList<String> colors = new ArrayList<String>();
+
+
+    }
+
+}
--- src/test/java/org/interviewquestion/ArrayListTest.java
+++ src/test/java/org/interviewquestion/ArrayListTest.java
@@ -1,4 +1,27 @@
 package org.interviewquestion;
 
+import org.junit.Test;
+
 public class ArrayListTest {
+
+    @Test
+    public void addShouldAdd(){
+
+    }
+
+
+
+
+    /*/
+    @Test
+    public void plusShouldAdd() {
+        Calculator calculator = new Calculator();
+        double num1 = 2;
+        double expected = num1 + calculator.getResult();
+        double actual = calculator.plus(num1);
+        assertEquals(expected, actual, precision);
+    }
+     */
+
+
 }
