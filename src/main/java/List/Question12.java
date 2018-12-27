030753fdb6095d96698ed465e8d145a27252d266
Hamza Demir <hamzademir4772@hotmail.com>
2018-12-27 21:25:31 +0000

    Completed Question 12

---
+++ src/main/java/List/Question12.java
@@ -0,0 +1,25 @@
+package List;
+
+//12.Write a Java program to extract a portion of a array list.
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question12 {
+
+    public static void main(String[] args){
+        ArrayList<String> colors = new ArrayList<String>();
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Blue");
+        colors.add("White");
+        colors.add("Black");
+
+        //LEARN HOW TO WRITE A SUBLIST
+        List<String> colorsSubList = colors.subList(2 , 4);
+
+        System.out.println(colorsSubList);
+    }
+}
