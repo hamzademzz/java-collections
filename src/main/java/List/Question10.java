5b7d252c42f0da4d0f829e1bf6e78508e82b018b
Hamza Demir <hamzademir4772@hotmail.com>
2018-12-27 21:18:22 +0000

    Completed Question 10

---
+++ src/main/java/List/Question10.java
@@ -0,0 +1,23 @@
+package List;
+
+//10. Write a Java program to shuffle elements in a array list.
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Question10 {
+
+    public static void main(String[] args){
+
+        ArrayList<String> colors = new ArrayList<String>();
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+
+        Collections.shuffle(colors);
+
+        System.out.println(colors);
+
+    }
+}
