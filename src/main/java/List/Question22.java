705a4000f4726c4d65185a84693086032035fab1
Hamza Demir <hamzademir4772@hotmail.com>
2018-12-28 00:13:48 +0000

    Completed Question 22

---
+++ src/main/java/List/Question22.java
@@ -0,0 +1,28 @@
+package List;
+
+//22. Write a Java program to print all the elements of a ArrayList using the position of the elements.
+
+import java.util.ArrayList;
+
+public class Question22 {
+
+    public static void main(String[] args){
+
+        ArrayList<String> colors = new ArrayList<String>();
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Blue");
+        colors.add("White");
+        colors.add("Black");
+
+
+        for(int i=0 ; i<colors.size(); i++){
+            System.out.println(colors.get(i));
+        }
+
+
+    }
+
+}
