0bc21a74d1097a975589160313a07ee709c04f9d
Hamza Demir <hamzademir4772@hotmail.com>
2018-12-28 00:00:48 +0000

    Completed Question 19

---
+++ src/main/java/List/Question19.java
@@ -0,0 +1,28 @@
+package List;
+
+//19. Write a Java program to trim the capacity of an array list the current list size.
+
+import java.util.ArrayList;
+
+public class Question19 {
+
+    public static void main(String[] args){
+
+        ArrayList<String> colors = new ArrayList<String>();
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Blue");
+        colors.add("White");
+        colors.add("Black");
+
+        System.out.println(colors);
+
+        colors.trimToSize();
+
+        System.out.println(colors);
+
+    }
+
+}
