17ea6b03657336fcff3bb7ac503abc47ca9375a6
Hamza Demir <hamzademir4772@hotmail.com>
2019-01-03 16:29:02 +0000

    Updated Java Coursework Task1

--- src/main/java/JavaAssignment/Task1.java
+++ src/main/java/JavaAssignment/Task1.java
@@ -3,6 +3,7 @@ package JavaAssignment;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,9 +11,9 @@ import java.util.Scanner;
 public class Task1 {
 
     public static void main(String[] args) throws FileNotFoundException {
-        //SuperHeroes heroes = new SuperHeroes();
+
         List<SuperHeroes> csvFileReader = new ArrayList<SuperHeroes>();
-        SuperHeroes hero = new SuperHeroes();
+        SuperHeroes heroes = new SuperHeroes();
 
         String pseudonym, realName, description;
         int height, intelligenceRating, strengthRating, speedRating, agilityRating, fightingSkillsRating;
@@ -30,7 +31,6 @@ public class Task1 {
                 while (input.hasNext()) {
                     int i = 0;
                     pseudonym = input.next();
-
                     height = Integer.parseInt(input.next());
                     intelligenceRating = Integer.parseInt(input.next());
                     strengthRating = Integer.parseInt(input.next());
@@ -41,22 +41,75 @@ public class Task1 {
                     description = input.next();
 
                     csvFileReader.add(i, new SuperHeroes(pseudonym, height, intelligenceRating, strengthRating, speedRating, agilityRating, fightingSkillsRating, realName, description));
-                    System.out.println(csvFileReader.size());
                     i++;
                 }
                 input.close();
-
-
             }
-            System.out.println(csvFileReader.size());
-        } catch (NumberFormatException exp) {
+
+
+        }
+        catch (NumberFormatException exp) {
+
+        }
+        List<Integer> heights = new ArrayList<Integer>();
+        for(int i=0; i<csvFileReader.size(); i++){
+            heights.add(csvFileReader.get(i).getHeight());
+        }
+
+
+        Collections.sort(heights);
+        Collections.reverse(heights);
+
+        List<Integer> topScores = new ArrayList<Integer>();
+        for(int i=0; i<5; i++){
+            topScores.add(heights.get(i));
+        }
+
+
+        List<Integer> heights2= new ArrayList<Integer>();
+        for(int i=0; i<csvFileReader.size(); i++){
+            heights2.add(csvFileReader.get(i).getHeight());
+        }
+
+        for(int i=0; i<5; i++){
+            System.out.println("Pseudonym " + csvFileReader.get(heights2.indexOf(topScores.get(i))).getPseudonym() +" (" + csvFileReader.get(heights2.indexOf(topScores.get(i))).getRealName()+ ")," + " Height = " + topScores.get(i) +" centimetres");
 
         }
 
 
+        System.out.println("");
 
-        System.out.println(csvFileReader.get(0));
+        List<Integer> strenthRatings = new ArrayList<Integer>();
+        for(int i=0; i<csvFileReader.size(); i++){
+            strenthRatings.add(csvFileReader.get(i).getStrengthRating());
+        }
+
+
+
+        Collections.sort(strenthRatings);
+
+
+
+        List<Integer> lowScores = new ArrayList<Integer>();
+        for(int i=0; i<5; i++){
+            lowScores.add(strenthRatings.get(i));
+        }
+
+
+        List<Integer> strenthRatings2= new ArrayList<Integer>();
+        for(int i=0; i<csvFileReader.size(); i++){
+            strenthRatings2.add(csvFileReader.get(i).getStrengthRating());
+        }
+
+
+        for(int i=0; i<5; i++){
+            System.out.println("Pseudonym " + csvFileReader.get(strenthRatings2.indexOf(lowScores.get(i))).getPseudonym() +" (" + csvFileReader.get(strenthRatings2.indexOf(lowScores.get(i))).getRealName()+ ")," + " Strength Rating = " + lowScores.get(i));
+        }
+
+
 
     }
 
 }
+
+
